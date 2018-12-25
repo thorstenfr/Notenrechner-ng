33e3b1580fe58250b5c13a950660a69de7764205
Thorsten Freimann <tfreimann@gmx.de>
2018-12-25 10:52:07 +0100

    Toggel linearer Notenschlüssel

--- js/controllers.js
+++ js/controllers.js
@@ -355,11 +355,11 @@ function ($scope, $rootScope, $stateParams, $ionicPopup, Data ) {
 }])
 
    
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuCtrl', ['$scope', '$stateParams', 'Data', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, Data) {
+	$scope.Data = Data;
 
 }])
  
